9afa4e41c33efb4effd72c72f73db8aca2c41bad
Michel Martineau <mmartineau@talend.com>
2024-02-26 13:34:21 +0100

    first occurence

--- leetcode/28-first-occurence_test.go
+++ leetcode/28-first-occurence_test.go
@@ -5,14 +5,14 @@ import (
 	"testing"
 )
 
-//func strStr(haystack string, needle string) int {
-//    for i:= 0; i <= len(haystack) - len(needle); i++ {
-//        if haystack[i:len(needle)+i] == needle {
-//            return i
-//        }
-//    }
-//    return -1
-//}
+//	func strStr(haystack string, needle string) int {
+//	   for i:= 0; i <= len(haystack) - len(needle); i++ {
+//	       if haystack[i:len(needle)+i] == needle {
+//	           return i
+//	       }
+//	   }
+//	   return -1
+//	}
 func firstOcc(haystack string, needle string) int {
 
 	if strings.HasPrefix(haystack, needle) {
